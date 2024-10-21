59b96c83c4060633fe1557be0d9fa06815807916
Piotr Cembrowski <losttape@gmail.com>
2024-10-21 14:42:42 +0200

    graphql schema.py for blog added

---
+++ back_end/blog/schema.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from graphene_django import DjangoObjectType
+
+from blog import models
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+        
+class AuthorType(DjangoObjectType):
+    class Meta:
+        model = models.Profile
+        
+class PostType(DjangoObjectType):
+    class Meta:
+        model = models.Post
+        
+class TagType(DjangoObjectType):
+    class Meta:
+        model = models.Tag
