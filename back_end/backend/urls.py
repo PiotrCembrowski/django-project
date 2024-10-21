5fc53538fb1e4210901c46c4965637641bfb97d9
Piotr Cembrowski <losttape@gmail.com>
2024-10-21 16:06:16 +0200

    adding path for post views

--- back_end/backend/urls.py
+++ back_end/backend/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.post_list, name='post_list'),
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
